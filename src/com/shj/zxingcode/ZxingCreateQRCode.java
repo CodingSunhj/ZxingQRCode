5cf2b61998b03f69b4bfa73d78329ecf465c4f0d
CodingSunhj <445677930@qq.com>
2017-05-12 08:43:41 +0800

    根据Zxing.jar完成的二维码生成与解析工具类 可生成带logo的二维码

---
+++ src/com/shj/zxingcode/ZxingCreateQRCode.java
@@ -0,0 +1,306 @@
+package com.shj.zxingcode;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64OutputStream;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class ZxingCreateQRCode {
+
+    public static final String QRCODE_DEFAULT_CHARSET = "UTF-8";
+
+    public static final int QRCODE_DEFAULT_HEIGHT = 150;
+
+    public static final int QRCODE_DEFAULT_WIDTH = 150;
+
+    private static final int BLACK = 0xFF000000;
+    private static final int WHITE = 0xFFFFFFFF;
+
+    /**
+     * 默认配置生成二维码图片
+     * 
+     * @author shj
+     * @param data
+     * @return
+     */
+    public static BufferedImage createQRCode(String data) {
+        return createQRCode(data, QRCODE_DEFAULT_WIDTH, QRCODE_DEFAULT_HEIGHT);
+    }
+
+    /**
+     * Create qrcode with default charset
+     *
+     * @author shj
+     * @param data
+     * @param width
+     * @param height
+     * @return
+     */
+    public static BufferedImage createQRCode(String data, int width, int height) {
+        return createQRCode(data, QRCODE_DEFAULT_CHARSET, width, height);
+    }
+
+    /**
+     * Create qrcode with specified charset
+     *
+     * @author shj
+     * @param data
+     * @param charset
+     * @param width
+     * @param height
+     * @return
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static BufferedImage createQRCode(String data, String charset, int width, int height) {
+        Map hint = new HashMap();
+        hint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hint.put(EncodeHintType.CHARACTER_SET, charset);
+
+        return createQRCode(data, charset, hint, width, height);
+    }
+
+    /**
+     * Create qrcode with specified hint
+     *
+     * @author shj
+     * @param data
+     * @param charset
+     * @param hint
+     * @param width
+     * @param height
+     * @return
+     */
+    public static BufferedImage createQRCode(String data, String charset, Map<EncodeHintType, ?> hint, int width,
+            int height) {
+        BitMatrix matrix;
+        try {
+            matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE,
+                    width, height, hint);
+            return toBufferedImage(matrix);
+        } catch (WriterException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    public static BufferedImage toBufferedImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, matrix.get(x, y) ? BLACK : WHITE);
+            }
+        }
+        return image;
+    }
+    /**
+     *默认配置生成带logo的二维码图片
+     *
+     * @author shj
+     * @param data
+     * @param logoFile
+     * @return
+     */
+    
+    public static BufferedImage createQRCodeWithLogo(String data, File logoFile) {
+        return createQRCodeWithLogo(data, QRCODE_DEFAULT_WIDTH, QRCODE_DEFAULT_HEIGHT, logoFile);
+    }
+
+    /**
+     * Create qrcode with default charset and logo
+     *
+     * @author shj
+     * @param data
+     * @param width
+     * @param height
+     * @param logoFile
+     * @return
+     */
+    public static BufferedImage createQRCodeWithLogo(String data, int width, int height, File logoFile) {
+        return createQRCodeWithLogo(data, QRCODE_DEFAULT_CHARSET, width, height, logoFile);
+    }
+
+    /**
+     * Create qrcode with specified charset and logo
+     *
+     * @author shj
+     * @param data
+     * @param charset
+     * @param width
+     * @param height
+     * @param logoFile
+     * @return
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public static BufferedImage createQRCodeWithLogo(String data, String charset, int width, int height, File logoFile) {
+        Map hint = new HashMap();
+        hint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hint.put(EncodeHintType.CHARACTER_SET, charset);
+
+        return createQRCodeWithLogo(data, charset, hint, width, height, logoFile);
+    }
+
+    /**
+     * Create qrcode with specified hint and logo
+     *
+     * @param data
+     * @param charset
+     * @param hint
+     * @param width
+     * @param height
+     * @param logoFile
+     * @return
+     */
+    public static BufferedImage createQRCodeWithLogo(String data, String charset, Map<EncodeHintType, ?> hint,
+            int width, int height, File logoFile) {
+        try {
+        	
+            BufferedImage qrcode = createQRCode(data, charset, hint, width, height);
+            String path = logoFile.getAbsolutePath();
+            String name = path.substring(0,path.indexOf("."))+"_zip";
+            String suffix = path.substring(path.indexOf("."));
+            String newFilePath = name+suffix;
+            //此处默认压缩�?30*30的logo形式，后期使用可以添加相应的方法进行修改
+            zipWidthHeightImageFile(logoFile, new File(newFilePath), 30, 30, 1);
+
+            File newFile = new File(newFilePath);
+            InputStream is = new FileInputStream(newFile);
+            BufferedImage logo = ImageIO.read(is);
+          int deltaHeight = height - logo.getHeight();
+          int deltaWidth = width - logo.getWidth();
+            BufferedImage combined = new BufferedImage(height, width, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = (Graphics2D) combined.getGraphics();
+            g.drawImage(qrcode, 0, 0, null);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+            g.drawImage(logo, (int) Math.round(deltaWidth / 2), (int) Math.round(deltaHeight / 2), null);
+
+            return combined;
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+    
+
+    /**
+     * Return base64 for image
+     *
+     * @author shj
+     * @param image
+     * @return
+     */
+    public static String getImageBase64String(BufferedImage image) {
+        String result = null;
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            OutputStream b64 = new Base64OutputStream(os);
+            ImageIO.write(image, "png", b64);
+            result = os.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        return result;
+    }
+
+    /**
+     * Decode the base64Image data to image
+     *
+     * @author shj
+     * @param base64ImageString
+     * @param file
+     */
+    public static void convertBase64StringToImage(String base64ImageString, File file) {
+        FileOutputStream os;
+        try {
+            Base64 d = new Base64();
+            byte[] bs = d.decode(base64ImageString);
+            os = new FileOutputStream(file.getAbsolutePath());
+            os.write(bs);
+            os.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    
+    /**  
+     * 按设置的宽度高度压缩图片文件<br> 先保存原文件，再压缩、上�?  
+     * @param oldFile  要进行压缩的文件全路�?  
+     * @param newFile  新文�?  
+     * @param width  宽度  
+     * @param height 高度  
+     * @param quality 质量  
+     * @return 返回压缩后的文件的全路径  
+     */    
+    public static String zipWidthHeightImageFile(File oldFile,File newFile, int width, int height,float quality) {    
+        if (oldFile == null) {    
+            return null;    
+        }    
+        String newImage = null;    
+        try {    
+            /** 对服务器上的临时文件进行处理 */    
+            Image srcFile = ImageIO.read(oldFile);    
+              
+            String srcImgPath = newFile.getAbsoluteFile().toString();  
+            String subfix = "jpg";  
+            subfix = srcImgPath.substring(srcImgPath.lastIndexOf(".")+1,srcImgPath.length());  
+  
+            BufferedImage buffImg = null;   
+            if(subfix.equals("png")){  
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);  
+            }else{  
+                buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);  
+            }  
+            Graphics2D graphics = buffImg.createGraphics();  
+            graphics.setBackground(new Color(255,255,255));  
+            graphics.setColor(new Color(255,255,255));  
+            graphics.fillRect(0, 0, width, height);  
+            graphics.drawImage(srcFile.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0, 0, null);    
+            ImageIO.write(buffImg, subfix, new File(srcImgPath));    
+        } catch (FileNotFoundException e) {    
+            e.printStackTrace();    
+        } catch (IOException e) {    
+            e.printStackTrace();    
+        }    
+        return newImage;    
+    }    
+
+}
---
+++ src/com/shj/zxingcode/ZxingDecode.java
@@ -0,0 +1,38 @@
+package com.shj.zxingcode;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+public class ZxingDecode {
+	 public String decodeQr(String filePath) {
+	        String retStr = "";
+	        if ("".equalsIgnoreCase(filePath) && filePath.length() == 0) {
+	            return "图片路径为空!";
+	        }
+	        try {
+	            BufferedImage bufferedImage = ImageIO.read(new FileInputStream(filePath));
+	            LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+	            Binarizer binarizer = new HybridBinarizer(source);
+	            BinaryBitmap bitmap = new BinaryBitmap(binarizer);
+	            HashMap<DecodeHintType, Object> hintTypeObjectHashMap = new HashMap<>();
+	            hintTypeObjectHashMap.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+	            Result result = new MultiFormatReader().decode(bitmap, hintTypeObjectHashMap);
+	            retStr = result.getText();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return retStr;
+	    }
+	 
+}
